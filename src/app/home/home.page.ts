4f2f5ce961fd08c117428122bd8549fdeb2a0cda
unknown <jeantituana@hotmail.com>
2023-12-06 16:06:30 -0500

    gps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//3
+
+import {Geolocation} from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  latitude: any=0;
+  longitude: any=0;
 
-  constructor() {}
+  constructor(private geolocation: Geolocation) {
+  }
+
+  options={
+    timeout:10000,
+    enableHighAccuracy: true,
+    maximumAge:3600
+  };
+
+  getCurrentCoordinates(){
+    this.geolocation.getCurrentPosition().then((resp)=>{
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+    }).catch((error) => {
+      console.log("Error, no se puede obtener su ubicacion: ", error);
+    });
+
+  };
+
+
+  
+
+
+
 
 }
